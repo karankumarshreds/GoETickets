213286c5de2c08f97543515e0677987bccfabfc8
karankumarshreds <naileditnerd@gmail.com>
2022-09-30 13:17:29 +0530

    setup logs with env variables

--- cmd/main.go
+++ cmd/main.go
@@ -1,9 +1,10 @@
 package main
 
 import (
-	"fmt"
+	"os"
 	"log"
 	"path/filepath"
+	"github.com/joho/godotenv"
 	"github.com/karankumarshreds/GoRedis/cmd/app"
 	"github.com/karankumarshreds/GoRedis/internal/common"
 )
@@ -11,13 +12,26 @@ import (
 const LOG_FILE_NAME = "logs.log"
 
 func main() {
+
+	// environment variabls setup
+	err := godotenv.Load(".env")
+	if err != nil {
+		log.Fatal("Cannot load the env file")
+	}
+
+	// logger setup 
 	absPath, err := filepath.Abs("./log")
 	if err != nil {
 		log.Println("Error generating absPath", err)
 	} 
-	
-	logger := common.NewLogger(absPath, LOG_FILE_NAME)
+	logFile := os.Getenv("LOGS_FILE")
+	if logFile == "" {
+		log.Fatal("Environment variable for LOGS_FILE not set")
+	}
+	logger := common.NewLogger(absPath, logFile)
+
+	// app setup
 	a := app.NewApp(logger)
 	a.Init()
-	fmt.Println("Starting app")
+	
 }
--- go.mod
+++ go.mod
@@ -5,4 +5,5 @@ go 1.16
 require (
 	github.com/go-redis/redis/v9 v9.0.0-beta.2 // indirect
 	github.com/golang-jwt/jwt v3.2.2+incompatible // indirect
+	github.com/joho/godotenv v1.4.0 // indirect
 )
