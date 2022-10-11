27a6fdcb5d99abbe85bcf02863e4bc79fe611e1f
karankumarshreds <illuminatekaran@gmail.com>
2022-10-11 08:26:35 +0530

    add redis connection

--- cmd/app/app.go
+++ cmd/app/app.go
@@ -1,15 +1,22 @@
 package app
 
 import (
+	"context"
 	"log"
 	"net/http"
+	"os"
+
+	"github.com/go-redis/redis/v9"
 	"github.com/gorilla/mux"
 	"github.com/karankumarshreds/GoRedis/internal/handlers"
 )
 
+var ctx = context.Background()
+
 type App struct {
 	logger *log.Logger
 	router *mux.Router
+	redis  *redis.Client
 }
 
 const PORT = ":8000"
@@ -20,6 +27,7 @@ func NewApp(logger *log.Logger) *App {
 
 func (a *App) Init() {
 	a.logger.Println("Initializing app")
+	a.initRedisClient()
 	a.router = mux.NewRouter()
 	a.initRoutes()
 }
@@ -34,4 +42,21 @@ func (a *App) Run() {
 func (a *App) initRoutes() {
 	authHandlers := handlers.NewAuthHandlers(a.logger)
 	a.router.HandleFunc("/api/signin", authHandlers.Signin).Methods("POST")
+}
+
+func (a *App) initRedisClient() {
+	client := redis.NewClient(
+		&redis.Options{
+			Addr:     os.Getenv("REDIS_HOST") + ":" + os.Getenv("REDIS_PORT"),
+			Password: os.Getenv("REDIS_PASSWORD"),
+			DB:       0, // USE DEFAULT DB FOR NOW
+		},
+	)
+	a.redis = client
+	_, err := a.redis.Get(ctx, "test").Result()
+	if err != redis.Nil {
+		a.logger.Fatal("Redis connection failed", err)
+	} else {
+		a.logger.Println("Redis connection successful")
+	}
 }
--- go.mod
+++ go.mod
@@ -3,7 +3,7 @@ module github.com/karankumarshreds/GoRedis
 go 1.16
 
 require (
-	github.com/go-redis/redis/v9 v9.0.0-beta.2 // indirect
+	github.com/go-redis/redis/v9 v9.0.0-beta.3 // indirect
 	github.com/golang-jwt/jwt v3.2.2+incompatible // indirect
 	github.com/gorilla/mux v1.8.0 // indirect
 	github.com/joho/godotenv v1.4.0 // indirect
